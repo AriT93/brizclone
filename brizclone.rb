60001f79c25d804b99ce44d9716f9f92577d6949
Ari Turetzky <abturet@edipa.(none)>
2010-03-24 20:49:20 -0500

    posting enabled

--- brizclone.rb
+++ brizclone.rb
@@ -6,10 +6,11 @@ require 'dm-core'
 require 'digest/sha1'
 require 'twitter'
 require 'sinatra-authentication'
-require 'pp'
 require 'haml'
+require 'rack-flash'
 
 use Rack::Session::Cookie, :secret => 'mah sekrit is 7 proxies oh yeah!!'
+use Rack::Flash, :sweep => true
 
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/users.db"
 )
@@ -23,12 +24,14 @@ class BcUser
   property :asecret, String
 end
 
-
 configure do
   @@config = YAML.load_file("config.yml") rescue nil || { }
-  @@oauth ||= Twitter::OAuth.new(@@config['consumer_key'],@@config['consumer_secret'], :sign_in => true)
+  @@oauth ||= Twitter::OAuth.new(@@config['consumer_key'],@@config['consumer_secret'],:sign_in => true)
   @@profile = nil
 end
+
+
+
 
 helpers do
   def partial(name, options={})
@@ -54,6 +57,7 @@ get '/' do
   haml :home
 end
 
+
 get '/auth' do
   @@oauth.authorize_from_request(session['rtoken'],session['rsecret'],params[:oauth_verifier])
   @@profile = Twitter::Base.new(@@oauth)
@@ -62,11 +66,9 @@ get '/auth' do
   session['asecret'] = @@oauth.access_token.secret
   user = BcUser.first(:email => current_user.email)
   if user == nil
-    puts "hdfsadkfhskdfhskdfh"
     user = BcUser.new()
     user.email = current_user.email
     if user.atoken == nil
-      puts "owiwrjhklshf"
     user.atoken = @@oauth.access_token.token
     user.asecret = @@oauth.access_token.secret
     end
@@ -80,6 +82,15 @@ get '/login' do
 end
 
 get '/twitter' do
+  if @@profile == nil
+    redirect '/'
+  end
   @tweets = @@profile.friends_timeline
   haml :twitter
+end
+
+post '/twitter' do
+  tweet = @@profile.update(params[:text])
+  flash[:notice] = "tweet ##{tweet.id} created"
+  redirect '/twitter'
 end
