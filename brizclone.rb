dd21753f8c83adda39bd14c5d25f2427471a5590
Ari Turetzky <abturet@arit93.(none)>
2010-03-25 16:44:30 -0500

    fixed class vars

--- brizclone.rb
+++ brizclone.rb
@@ -8,6 +8,7 @@ require 'twitter'
 require 'sinatra-authentication'
 require 'haml'
 require 'rack-flash'
+require 'pp'
 
 use Rack::Session::Cookie, :secret => 'mah sekrit is 7 proxies oh yeah!!'
 use Rack::Flash, :sweep => true
@@ -26,8 +27,6 @@ end
 
 configure do
   @@config = YAML.load_file("config.yml") rescue nil || { }
-  @@oauth ||= Twitter::OAuth.new(@@config['consumer_key'],@@config['consumer_secret'],:sign_in => true)
-  @@profile = nil
 end
 
 
@@ -39,40 +38,51 @@ helpers do
   end
 end
 
+before do
+  @oauth ||= Twitter::OAuth.new(@@config['consumer_key'],@@config['consumer_secret'],:sign_in => true)
+  @user = nil
+  next unless logged_in?
+  @user = BcUser.first(:email => current_user.email)
+  if @user != nil
+    @oauth.authorize_from_access(@user.atoken,@user.asecret)
+    @profile = Twitter::Base.new(@oauth)
+  else
+    @profile = nil
+  end
+end
+
+
 get '/' do
   redirect '/login' unless logged_in?
-
-  if session['atoken'] != ''
-    @@oauth.set_callback_url(@@config['callback_url'])
-    session['rtoken'] = @@oauth.request_token.token
-    session['rsecret'] = @@oauth.request_token.secret
-    user = BcUser.first(:email => current_user.email)
-    if user != nil
-      @@oauth.authorize_from_access(user.atoken,user.asecret)
-      @@profile = Twitter::Base.new(@@oauth)
-    else
-      redirect @@oauth.request_token.authorize_url
-    end
+  if session['atoken'] = ' '
+  if @user
+    @oauth.authorize_from_access(@user.atoken,@user.asecret)
+    @profile = Twitter::Base.new(@oauth)
+  else
+    @oauth.set_callback_url(@@config['callback_url'])
+    session['rtoken'] = @oauth.request_token.token
+    session['rsecret'] = @oauth.request_token.secret
+    redirect @oauth.request_token.authorize_url
+  end
   end
   haml :home
 end
 
 
 get '/auth' do
-  @@oauth.authorize_from_request(session['rtoken'],session['rsecret'],params[:oauth_verifier])
-  @@profile = Twitter::Base.new(@@oauth)
-  @@profile.verify_credentials
-  session['atoken'] = @@oauth.access_token.token
-  session['asecret'] = @@oauth.access_token.secret
-  user = BcUser.first(:email => current_user.email)
-  if user == nil
-    user = BcUser.new()
-    user.email = current_user.email
-    if user.atoken == nil
-    user.atoken = @@oauth.access_token.token
-    user.asecret = @@oauth.access_token.secret
+  @oauth.authorize_from_request(session['rtoken'],session['rsecret'],params[:oauth_verifier])
+  @profile =  Twitter::Base.new(@oauth)
+  @profile.verify_credentials
+  session['atoken'] = @oauth.access_token.token
+  session['asecret'] = @oauth.access_token.secret
+  if @user == nil
+    @user = BcUser.new()
+    @user.email = current_user.email
+    if @user.atoken == nil
+    @user.atoken = @oauth.access_token.token
+    @user.asecret = @oauth.access_token.secret
     end
-    user.save
+    @user.save
   end
   redirect '/'
 end
@@ -82,15 +92,13 @@ get '/login' do
 end
 
 get '/twitter' do
-  if @@profile == nil
-    redirect '/'
-  end
-  @tweets = @@profile.friends_timeline
+  redirect '/' unless @profile
+  @tweets = @profile.friends_timeline
   haml :twitter
 end
 
 post '/twitter' do
-  tweet = @@profile.update(params[:text])
+  tweet = @profile.update(params[:text])
   flash[:notice] = "tweet ##{tweet.id} created"
   redirect '/twitter'
 end
